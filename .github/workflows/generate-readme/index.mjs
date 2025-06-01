42061142a104a68d58875cbf8a80ad7453876528
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2025-06-01 21:40:01 +0200

    Update README.md Template #19

--- .github/workflows/generate-readme/index.mjs
+++ .github/workflows/generate-readme/index.mjs
@@ -3,26 +3,21 @@ import { SchemaImporter } from './../../../src/index.mjs'
 import { Print } from './src/task/Print.mjs'
 import fs from 'fs'
 
-/*
-    const files = await Files.getAllFiles( { dir: './schemas/v1.2.0/' } )
-    const preparedParams = await Files.getSchemaDetails( { files } )
-*/
 
-await SchemaImporter
-    .init( { 
+const preparedParams = await SchemaImporter
+    .get( { 
         schemaRootFolder:"../schemas/v1.2.0/", 
-        onlyWithoutImports:false,
-        withMetaData:true, 
-        withSchema:true
+        onlyWithoutImports: false,
+        withMetaData: true, 
+        withSchema: true
     } )
-const preparedParams = SchemaImporter.get()
-console.log( `Schemas imported: ${preparedParams.length}` )
 
+console.log( `Schemas imported: ${preparedParams.length}` )
 const { table } = Print.getTable( { preparedParams } )
 const { commandStr } = Print.getCommand( { preparedParams } )
 
-
-const readme = fs.readFileSync( './.github/workflows/generate-readme/src/templates/README.md', 'utf8' )
+const readme = fs
+    .readFileSync( './.github/workflows/generate-readme/src/templates/README.md', 'utf8' )
     .replace( '{{INSERT_TABLE}}', table )
     .replace( '{{INSERT_COMMAND_STRING}}', commandStr )
     
